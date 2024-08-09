4c3629b4d184ca22277d0670f2046e280e816703
RajashekharReddyBheemreddy <bheemreddyrajashekhar@gmail.com>
2024-08-08 22:42:06 -0400

    added my hotels view

--- backend/src/models/hotel.ts
+++ backend/src/models/hotel.ts
@@ -1,5 +1,21 @@
 import mongoose from "mongoose";
-import {HotelType} from '../../../frontend/src/shared/types';
+
+export type HotelType = {
+    _id: string;
+    userId: string;
+    name: string;
+    city: string;
+    country: string;
+    description: string;
+    type: string;
+    adultCount: number;
+    childCount: number;
+    facilities: string[];
+    starRating: number;
+    imageUrls: string[];
+    lastUpdated: Date;
+    pricePerNight:number;
+};
 
 const hotelSchema = new mongoose.Schema<HotelType>({
     userId: {
